846f37a67d0adca4a026fdbe0d18ebe2af6bedb7
pratheep MM <pratheepm@sahaj.ai>
2023-10-22 11:33:02 +0530

    bug fix: compile proto buf

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_files = vec![
-        "proto/health_check.proto",
-        "proto/probe.proto",
-    ];
-
-    tonic_build::configure()
-        .compile(&proto_files, &["proto/"])?;
+    // let proto_files = vec![
+    //     "proto/health_check.proto",
+    //     "proto/probe_sync.proto",
+    // ];
+    //
+    // tonic_build::configure()
+    //     .compile(&proto_files, &["proto/"])?;
+    tonic_build::compile_protos("./protos/probe_sync.proto")?;
 
     Ok(())
 }
