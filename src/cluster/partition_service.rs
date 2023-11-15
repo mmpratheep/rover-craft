e4778669168ec33ece4fa507ecf2ff4b36106fc8
pratheep MM <pratheepm@sahaj.ai>
2023-11-15 22:43:32 +0530

    balance partitions

--- src/cluster/partition_service.rs
+++ src/cluster/partition_service.rs
@@ -1,15 +1,18 @@
 use std::sync::Arc;
 use tonic::{Request, Response, Status};
 use crate::cluster::hash::hash;
+use crate::grpc::leader_node::LeaderNode;
 use crate::grpc::node::Node;
-use crate::grpc::service::cluster::{Empty, Partitions};
+use crate::grpc::node_status::NodeStatus;
+use crate::grpc::service::cluster::{AliveNotServingRequest, Empty};
 use crate::grpc::service::cluster::partition_server::Partition;
+use crate::store::memory_store::MemoryStore;
 
 
 #[derive(Debug, Default)]
 pub(crate) struct PartitionService {
     //todo store the reference of Nodes in leader/follower nodes
-    leader_nodes: Vec<Arc<Node>>,
+    leader_nodes: Vec<LeaderNode>,
     follower_nodes: Vec<Arc<Node>>,
     partition_size: usize,
 }
@@ -19,7 +22,7 @@ impl PartitionService {
         //distributes partition evenly across nodes with replication factor n-1
         let replica = nodes.len() - 1;
         let size = nodes.len() * replica;
-        let mut leader_nodes: Vec<Arc<Node>> = Vec::with_capacity(size);
+        let mut leader_nodes: Vec<LeaderNode> = Vec::with_capacity(size);
         let mut follower_nodes: Vec<Arc<Node>> = Vec::with_capacity(size);
         if replica < nodes.len() {
             let mut index: usize = 0;
@@ -33,33 +36,67 @@ impl PartitionService {
         self.leader_nodes = leader_nodes;
         self.follower_nodes = follower_nodes;
     }
-    fn remove_node(nodes: &mut Vec<Arc<Node>>, node: &Node) {
-        for i in 0..nodes.len() {
-            if node.address == nodes.get(i).unwrap().address {
-                nodes.remove(i);
-                return;
-            }
-        }
-    }
 
-    pub fn get_partition_nodes(&self, probe_id: &String) -> (Option<&Arc<Node>>, Option<&Arc<Node>>) {
+    // fn remove_node(nodes: &mut Vec<Arc<Node>>, node: &Node) {
+    //     for i in 0..nodes.len() {
+    //         if node.host_name == nodes.get(i).unwrap().host_name {
+    //             nodes.remove(i);
+    //             return;
+    //         }
+    //     }
+    // }
+
+    pub fn get_partition_nodes(&self, probe_id: &String) -> (Option<&LeaderNode>, Option<&Arc<Node>>) {
         let hash = hash(probe_id.clone());
         let partition_id = hash % self.partition_size;
         (self.leader_nodes.get(partition_id), self.follower_nodes.get(partition_id))
+    }
+
+    pub(crate) fn balance_partitions_and_write_delta_data(&mut self) {
+        for i in 0..self.leader_nodes.len() {
+            let leader_node = self.leader_nodes.get(i).unwrap();
+            let follower_node = self.follower_nodes.get(i).unwrap();
+            self.reassign_leader_partition_and_write_delta_data(i, leader_node, follower_node);
+
+            self.handle_follower_delta_data_write(i, follower_node);
+        }
+    }
+
+    fn reassign_leader_partition_and_write_delta_data(&mut self, i: usize, leader_node: &LeaderNode, follower_node: &Arc<Node>) {
+        if leader_node.node.node_status == NodeStatus::Dead {
+            let mut delta_data: Option<MemoryStore> = None;
+            if follower_node.is_current_node() {
+                delta_data = Some(MemoryStore::new());
+            }
+            self.leader_nodes[i] = LeaderNode { node: follower_node.clone(), delta_data: None };
+        }
+    }
+
+    fn handle_follower_delta_data_write(&mut self, i: usize, follower_node: &Arc<Node>) {
+        if follower_node.node_status == NodeStatus::Dead {
+            if follower_node.is_current_node() {
+                let mut leader_node = self.leader_nodes.get_mut(i).unwrap();
+                leader_node.delta_data = Some(Arc::new(MemoryStore::new()));
+            }
+        }
     }
 }
 
 #[tonic::async_trait]
 impl Partition for PartitionService {
-    async fn get_partitions_from_leader(&self, request: Request<Empty>) -> Result<Response<Partitions>, Status> {
-        todo!("handle logic for node recovery as well")
+    async fn make_node_alive_not_serving(&self, request: Request<AliveNotServingRequest>) -> Result<Response<Empty>, Status> {
+        todo!()
+    }
+
+    async fn make_node_alive_serving(&self, request: Request<Empty>) -> Result<Response<Empty>, Status> {
+        todo!()
     }
 }
 
 
 fn clone_except_given_value(nodes: &Vec<Node>, node: &Node) -> Vec<Node> {
     let mut temp_nodes = nodes.clone();
-    temp_nodes.retain(|n| n.address != node.address);
+    temp_nodes.retain(|n| n.host_name != node.host_name);
     return temp_nodes;
 }
 
@@ -72,9 +109,12 @@ fn assign_values_for_replica(replica: usize, follower_nodes: &mut Vec<Arc<Node>>
     }
 }
 
-fn assign_values_for_leader(replica: usize, leader_nodes: &mut Vec<Arc<Node>>, node: &Node, mut current_index: usize) {
+fn assign_values_for_leader(replica: usize, leader_nodes: &mut Vec<LeaderNode>, node: &Node, mut current_index: usize) {
     for _ in 0..replica {
-        leader_nodes.insert(current_index, Arc::new(node.clone()));
+        leader_nodes.insert(current_index, LeaderNode {
+            node: Arc::from(node.clone()),
+            delta_data: None,
+        });
         current_index += 1;
     }
 }
